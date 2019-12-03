a7e23e288559f46a424b3082c60101982a05ca4e
Sebastiaan van Arkens <sebastiaan.van.arkens@frontmen.nl>
2019-12-03 10:32:53 +0100

    Day three

---
+++ 03/part2.js
@@ -0,0 +1,59 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./input.txt");
+
+const wires = input
+  .toString()
+  .split("\n")
+  .map(wire => wire.trim().split(","));
+
+const createWireMap = wire => {
+  const positions = [];
+  const position = { x: 0, y: 0 };
+
+  let wireLength = 0;
+
+  for (let i = 0; i < wire.length; i++) {
+    const direction = wire[i][0];
+    const distance = Number(wire[i].slice(1));
+
+    for (let step = 1; step <= distance; step++) {
+      switch (direction) {
+        case "L":
+          position.x += -1;
+          break;
+        case "R":
+          position.x += 1;
+          break;
+        case "U":
+          position.y += -1;
+          break;
+        case "D":
+          position.y += 1;
+          break;
+      }
+
+      positions.push([`${position.x}, ${position.y}`, ++wireLength]);
+    }
+  }
+
+  return positions;
+};
+
+const wire1Positions = createWireMap(wires[0]);
+const wire2Positions = createWireMap(wires[1]);
+
+console.log(
+  wire2Positions
+    .filter(position => {
+      const [wire2Pos] = position;
+      return wire1Positions.find(([wire1Pos]) => wire1Pos === wire2Pos);
+    })
+    .map(([wire2Pos, steps]) => {
+      const [itemPos, itemSteps] = wire1Positions.find(
+        ([wire1Pos]) => wire1Pos === wire2Pos
+      );
+      return itemSteps + steps;
+    })
+    .sort((a, b) => a - b)[0]
+);
